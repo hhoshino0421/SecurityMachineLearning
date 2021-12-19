8b2c86b6785d65045361bdf6c4e580536ad083f8
Hoshino Hitoshi <hhoshino-main@memoad.jp>
2021-12-19 10:10:32 +0900

    CHAP03 sample code add. and sample data file add.(sample data file is  large size)

---
+++ ch3/SecurityMachineLearningCH3/main.py
@@ -0,0 +1,27 @@
+
+from MalwareProfile import *
+from MalwareFutureSelect import *
+from MalwareFutureRandomForest import *
+
+
+# メイン関数
+def main_func():
+
+    # マルウェアデータプロファイル
+    # malware_profile_main()
+
+    # 特徴量機械学習
+    # malware_future_select()
+
+    # ランダムフォレストのパラメータチューニング処理
+    study_obj, x_obj, y_obj = malware_future_select_random_forest_tuning()
+
+    # ランダムフォレストでの機械学習処理
+    malware_random_forest_learning(study_obj, x_obj, y_obj)
+
+
+
+# エントリポイント関数
+if __name__ == '__main__':
+    main_func()
+
