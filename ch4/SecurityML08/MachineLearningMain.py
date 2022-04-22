a6091178d01f8e34c030afebfb24ec11d8b6ec45
Hoshino Hitoshi <hhoshino-main@memoad.jp>
2022-04-22 10:48:28 +0900

    Debug code add. This code run.

--- ch4/SecurityML08/MachineLearningMain.py
+++ ch4/SecurityML08/MachineLearningMain.py
@@ -11,13 +11,25 @@ def check_malware(check_file_path, model_file_path):
 
     check_file_obj = read_file(check_file_path)
 
-    #scaler = StandardScaler()
+    scaler = StandardScaler()
+    scaler.fit(check_file_obj)
+    sample_data_new = scaler.transform(check_file_obj)
     #sample_data_new = scaler.transform(check_file_obj)
-    sample_data_new = StandardScaler.transform(check_file_obj)
+    #sample_data_new = StandardScaler.transform(check_file_obj)
 
     model_obj = read_model(model_file_path)
 
-    extractor = ember.PEFeatureExtractor(2)
+    # extractor = ember.PEFeatureExtractor(2)
+
+    pred = (model_obj.predict(sample_data_new) > 0.5).astype("int32")
+
+    # for debug
+    print(pred)
+
+    if pred:
+        print("Malware!")
+    else:
+        print("benign file")
 
     print("end")
 
